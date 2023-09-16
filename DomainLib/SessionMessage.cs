860d46b93271e30609cb399fd30981b2f17612ac
Luan Carlo <luanveloz@gmail.com>
2023-09-16 16:39:26 +0300

    refactor(DomainLib): namespaces

---
+++ DomainLib/SessionMessage.cs
@@ -0,0 +1,10 @@
+namespace DomainLib
+{
+    public class SessionMessage
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
